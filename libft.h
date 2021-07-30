cf0cf6676b494ffc72ef175e8f5112abcb71bf83
Anderson <acarneirosousa@gmail.com>
2021-07-30 18:37:26 -0300

    strlcpy

--- libft.h
+++ libft.h
@@ -28,6 +28,7 @@ int		ft_isalpha(int c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
