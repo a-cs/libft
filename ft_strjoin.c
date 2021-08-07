37fb47833f2b52fdfbd18357e318428fd7ed5325
Anderson <acarneirosousa@gmail.com>
2021-08-06 23:03:20 -0300

    fixing strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -20,7 +20,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	str = (char *)calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
+	str = (char *)ft_calloc(ft_strlen(s1) + ft_strlen(s2) + 1, sizeof(char));
 	if (!str)
 		return (NULL);
 	pos1 = 0;
